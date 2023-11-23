788514a4d1025a81f18fc28c0d72d475baeed82f
Noel Maniawasi <150312009+noelckristabel@users.noreply.github.com>
2023-11-23 11:32:22 +0900

    Add files via upload

---
+++ comments.php
@@ -0,0 +1,11 @@
+<?php
+	// contoh komentar single-line di PHP
+
+	#ini juga contoh komentar single-line di php
+
+	/*
+	kalau yang ini merupakan contoh komentar mltiples-lines yang bida dalam sebuah file PHP 
+	*/
+
+	echo "PHP comments";
+?>
